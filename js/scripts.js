0b2dd9547a1afb4ef80a1d6dac4ab529713d5b46
Andrew Giang <giang184@gmail.com>
2021-06-04 14:13:47 -0700

    add branching structure

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,39 @@ $(document).ready(function() {
     const sleep = $("select#sleep").val();
     const animal = $("input:radio[name=animal]:checked").val();
     const color = $("input:radio[name=color]:checked").val();
-
     
-    $("#match").text(name + age+gender+sleep+animal+color);
-    $("#match").append(+ " " + color);
+    if(gender==='female') {
+      if(thing==='shopping' && color!=='blue' ){
+        statement = "Rachel";
+      }
+      else if(thing==='shopping' || thing==='eating' ) {
+        statement = "Monica";
+      }
+      else if (thing ==='smoking'){
+        statement = "Chandler";
+      }
+      else {
+        statement = "Phoebe";
+      }
+    }
+    else {
+      if(thing ==='eating'){
+        statement = "Joey";
+      }
+      else if(thing === 'smoking') {
+        statement = "Chandler";
+      }
+      else if(thing==='shopping' || color==='pink' ) {
+        statement = "Rachel";
+      }
+      else {
+        statement = "Ross";
+      }
+    }
+    
+
+
+    //$("#match").append( color);
     $("#result").show();
     event.preventDefault();
   });
